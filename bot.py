42ae8d9a66fe542805ea2a038be273e57f39181e
teluguanimes60-ops <teluguanimes60@gmail.com>
2026-06-15 22:19:58 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+app = Client("media_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text("🚀 Bot is running on Railway")
+
+app.run()
